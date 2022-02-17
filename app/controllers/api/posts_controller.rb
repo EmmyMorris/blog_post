67f1938aaf36f9f8eb0890281301f8134b1b4dda
Emmy Morris <emmymorris1102@gmail.com>
2022-02-17 12:57:53 -0700

    Add error handling for no tag

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,6 +1,9 @@
 class Api::PostsController < ApplicationController
   def index
-    @posts = PostsFacade.get_post_data(params[:tags], params[:soryBy], params[:direction])
-    render json: @posts, status: 200
+    if !params[:tags]
+      render json: { "error": "Tags parameter is required"}, status: 400
+    else @posts = PostsFacade.get_post_data(params[:tags], params[:soryBy], params[:direction])
+      render json: @posts, status: 200
+    end
   end
 end
--- spec/requests/api/post_request_spec.rb
+++ spec/requests/api/post_request_spec.rb
@@ -17,5 +17,31 @@ RSpec.describe "Post API" do
       expect(body[:posts][0]).to have_key(:author)
       expect(body[:posts][0][:author]).to be_a(String)
     end
+
+    context 'sad paths' do
+      it 'Forgets to add tag field', :vcr do
+
+        get "/api/posts?sortBy=popularity&direction=asc"
+
+        body = JSON.parse(response.body, symbolize_names: true)
+        # require "pry"; binding.pry
+        expect(response).to_not be_successful
+        expect(response.status).to be(400)
+        expect(body).to have_key(:error)
+        expect(body[:error]).to eq("Tags parameter is required")
+      end
+
+      xit 'Invalid sory by', :vcr do
+
+        get "/api/posts?tags=tech&sortBy=popularity&direction=hi"
+
+        body = JSON.parse(response.body, symbolize_names: true)
+        # require "pry"; binding.pry
+        expect(response).to_not be_successful
+        expect(response.status).to be(400)
+        expect(body).to have_key(:error)
+        expect(body[:error]).to eq("sortBy parameter is invalid")
+      end
+    end
   end
 end
