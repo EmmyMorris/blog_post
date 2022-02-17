b27b508530bf9082dbff8f0b21bfa7e0a92daec6
Emmy Morris <emmymorris1102@gmail.com>
2022-02-17 14:36:58 -0700

    Refactor and clean up

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,18 +1,9 @@
 class Api::PostsController < ApplicationController
   def index
-    if !params[:tag]
+    if !params[:tags]
       render json: { "error": "Tags parameter is required"}, status: 400
-    elsif params_not_valid(params[:soryBy], params[:direction])
-    else @posts = PostsFacade.get_post_data(params[:tag], params[:soryBy], params[:direction])
+    else @posts = PostsFacade.get_post_data(params[:tags], params[:sortBy] = "asc", params[:direction])
       render json: @posts, status: 200
-    end
-  end
-
-  private
-
-  def params_not_valid(soryBy, direction)
-    if soryBy != ("id" || "reads" || "likes" || "popularity") || :direction != ("asc" || "desc")
-      render json: { "error": "sortBy parameter is invalid" }
     end
   end
 end
--- spec/requests/api/post_request_spec.rb
+++ spec/requests/api/post_request_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "Post API" do
+RSpec.describe "Post Route Requests" do
 
   context 'happy paths' do
     it 'Sends posts in tag field, and post attributes', :vcr do
 
-      get "/api/posts?tag=tech&sortBy=popularity&direction=asc"
+      get "/api/posts?tags=tech&sortBy=popularity&direction=asc"
 
       body = JSON.parse(response.body, symbolize_names: true)
       expect(response).to be_successful
@@ -17,31 +17,42 @@ RSpec.describe "Post API" do
       expect(body[:posts][0]).to have_key(:author)
       expect(body[:posts][0][:author]).to be_a(String)
     end
+  end
+  context 'sad paths' do
+    it 'Forgets to add tag field', :vcr do
 
-    context 'sad paths' do
-      it 'Forgets to add tag field', :vcr do
+      get "/api/posts?sortBy=popularity&direction=asc"
 
-        get "/api/posts?sortBy=popularity&direction=asc"
+      body = JSON.parse(response.body, symbolize_names: true)
+      # require "pry"; binding.pry
+      expect(response).to_not be_successful
+      expect(response.status).to be(400)
+      expect(body).to have_key(:error)
+      expect(body[:error]).to eq("Tags parameter is required")
+    end
 
-        body = JSON.parse(response.body, symbolize_names: true)
-        # require "pry"; binding.pry
-        expect(response).to_not be_successful
-        expect(response.status).to be(400)
-        expect(body).to have_key(:error)
-        expect(body[:error]).to eq("Tags parameter is required")
-      end
+    xit 'Invalid direction', :vcr do
 
-      xit 'Invalid sory by', :vcr do
+      get "/api/posts?tags=tech&sortBy=popularity&direction=hi"
 
-        get "/api/posts?tag=tech&sortBy=popularity&direction=hi"
+      body = JSON.parse(response.body, symbolize_names: true)
+      # require "pry"; binding.pry
+      expect(response).to_not be_successful
+      expect(response.status).to be(400)
+      expect(body).to have_key(:error)
+      expect(body[:error]).to eq("sortBy parameter is invalid")
+    end
 
-        body = JSON.parse(response.body, symbolize_names: true)
-        # require "pry"; binding.pry
-        expect(response).to_not be_successful
-        expect(response.status).to be(400)
-        expect(body).to have_key(:error)
-        expect(body[:error]).to eq("sortBy parameter is invalid")
-      end
+    xit 'Invalid Sort By', :vcr do
+
+      get "/api/posts?tags=tech&sortBy=hi&direction=asc"
+
+      body = JSON.parse(response.body, symbolize_names: true)
+      # require "pry"; binding.pry
+      expect(response).to_not be_successful
+      expect(response.status).to be(400)
+      expect(body).to have_key(:error)
+      expect(body[:error]).to eq("sortBy parameter is invalid")
     end
   end
 end
